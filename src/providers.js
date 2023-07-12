b6ed06adf1cf8652294063d3d108ee2664b9efbf
EugeneVolkov <5811802-EugeneVolkov@users.noreply.replit.com>
2023-07-12 13:53:15 +0000

    Initial commit

---
+++ src/providers.js
@@ -0,0 +1,26 @@
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+
+const APP_NAME = "My Demo App";
+const APP_LOGO_URL = "https://example.com/logo.png";
+const INFURA_ID = process.env.INFURA_ID;
+const INFURA_RPC_URL = `https://mainnet.infura.io/v3/${INFURA_ID}`;
+const DEFAULT_CHAIN_ID = 1;
+
+// Coinbase Wallet Provider
+export const getCoinbaseWalletProvider = () => {
+  const coinbaseWallet = new CoinbaseWalletSDK({
+    appName: APP_NAME,
+    appLogoUrl: APP_LOGO_URL,
+    darkMode: false,
+    overrideIsMetaMask: false
+  });
+  return coinbaseWallet.makeWeb3Provider(INFURA_RPC_URL, DEFAULT_CHAIN_ID);
+};
+
+// MetaMask Provider
+export const getMetaMaskProvider = () => {
+  // We will prefer a provider where the property `isMetaMask` is set to true
+  return (
+    window.ethereum?.providers?.find((p) => !!p.isMetaMask) ?? window.ethereum
+  );
+};
